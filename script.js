7a9fcbaa23d2c55e3dea80ba231b642e8cf312e0
Hugo <hugogfernandes0012@gmail.com>
2023-11-14 10:22:23 -0300

    Code refactoring and clean-up

--- script.js
+++ script.js
@@ -2,30 +2,13 @@ const myLibrary = [];
 const containerDiv = document.getElementById("container");
 
 const dialog = document.querySelector("dialog");
-const openDialog = document.getElementById("new_book");
-const closeDialog = document.getElementById("close_dialog");
-const dialogTitle = document.querySelector("#title");
-const dialogAuthor = document.querySelector("#author");
-const dialogPages = document.querySelector("#pages");
-const dialogSubmit = document.querySelector("#submit");
-const dialogForm = document.querySelector("#form");
-
-dialogSubmit.addEventListener('click', (Event) => {
-	let book = new Book(dialogTitle.value, dialogAuthor.value, dialogPages.value);
-	addToPage(book);
-	dialogForm.reset();
-	dialog.close();
-
-	Event.preventDefault();
-});
-
-openDialog.addEventListener('click', () => {
-	dialog.showModal();
-});
-
-closeDialog.addEventListener('click', () => {
-	dialog.close();
-});
+const newBtn = document.getElementById("new_book");
+const closeForm = document.getElementById("close");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const submitBtn = document.querySelector("#submit");
+const form = document.querySelector("#form");
 
 function Book (title, author, pages) {
 	this.title = title;
@@ -36,25 +19,30 @@ function Book (title, author, pages) {
 function addToLibrary(book) {
 	myLibrary.push(book);
 }
+
 function addToPage(book) {
 	let para = document.createElement("p");
 	para.textContent = `${book.title} `;
 	containerDiv.appendChild(para);
-
 }
 
-const book1 = new Book("A Hora da Estrela", "Clarice Lispector", 100);
-const book2 = new Book("Um Sopro de Vida", "Clarice Lispector", 180);
-const book3 = new Book("A Obscena Senhora D", "Hilda Hilst", 50);
-const book4 = new Book("Mrs. Dalloway", "Virginia Woolf", 250);
-const book5 = new Book("Vidas Secas", "Graciliano Ramos", 150);
+submitBtn.addEventListener('click', (Event) => {
+	let book = new Book(title.value, author.value, pages.value);
+	addToLibrary(book);
+	addToPage(book);
+	form.reset();
+	dialog.close();
 
-addToLibrary(book1);
-addToLibrary(book2);
-addToLibrary(book3);
-addToLibrary(book4);
-addToLibrary(book5);
+	Event.preventDefault();
+});
 
-myLibrary.forEach((book) => addToPage(book));
+newBtn.addEventListener('click', () => {
+	dialog.showModal();
+});
 
-console.log(myLibrary);
+closeForm.addEventListener('click', (Event) => {
+	form.reset();
+	dialog.close();
+
+	Event.preventDefault();
+});
